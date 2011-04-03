98474786b2de04ed94a5488e722d447559444411
zpoley <zpoley@gmail.com>
2011-04-02 21:32:48 -0700

    () update tests.

--- test.py
+++ test.py
@@ -1,29 +1,45 @@
 #!/usr/bin/env python
 
-import unittest
+import os, commands, random, unittest
 
 class TestMatchOutput(unittest.TestCase):
   def setUp(self):
     pass
 
-  def testMatchOutput(self):
-    print "testMatchOutput:"
+  def testIdenticalLines(self):
+    print "testIdenticalLines:"
 
-'''
-//  char *filename1 = "data/test_file_1.txt";
-//  char *filename2 = "data/test_file_2.txt";
+    # build fcompare
+    os.system("make")
 
-//  char *filename1 = "data/names.1.100.txt";
-//  char *filename2 = "data/names.2.100.txt";
+    f1_name = "data/f1_test.txt"
 
-//  char *filename1 = "data/names.3.100.txt";
-//  char *filename2 = "data/names.4.100.txt";
+    # quickly generate two files with a few differences
+    f1 = open(f1_name, "w")
 
-//  char *filename1 = "data/names.5.100K.txt";
-//  char *filename2 = "data/names.6.100K.txt";
-'''
+    for i in xrange(0, 100): # 100 lines
+      for j in xrange(0, random.randint(0, 255)):
+        f1.write("%s" % chr(random.randint(14, 127)))
+      f1.write("%s\n" % str)
 
+    f1.close()
 
+    res = commands.getoutput("./fcompare %s %s" % (f1_name, f1_name))
+
+    # assert comparing a file to itself produces no output
+    self.assertEqual(res, "")
+
+    os.remove(f1_name)
+
+  def testDifferentLines(self):
+    print "testDifferentLines:"
+
+    res = commands.getoutput("./fcompare data/test_file_1.txt data/test_file_2.txt")
+
+    diffs = [ "abc", "foo", "bar" ]
+
+    for line in res.split("\n"):
+      self.assertTrue(line in diffs)
 
 if __name__ == '__main__':
   unittest.main() 
